d537350ddf330cd8fd505f38a3b253793d249e8a
Brandon-Mount <bmount44@gmail.com>
2025-01-10 10:05:12 -0700

    added code to create a magic number game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,33 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+       // here we will get a random number between 1 and 100
+         Random random = new Random();
+         int magicNumber = random.Next(1, 101);
+
+         int guess = 0;
+
+            while (guess != magicNumber)
+            {
+                Console.WriteLine("Guess the magic number between 1 and 100: ");
+                guess = int.Parse(Console.ReadLine());
+    
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("The magic number is greater than your guess.");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("The magic number is less than your guess.");
+                }
+                else
+                {
+                    Console.WriteLine("Congratulations! You guessed the magic number!");
+                }
+            }
+
+
+
     }
 }
